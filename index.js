914ce8ed06b3dffcadc485786ea7f5e23f3528fa
adityasinha135 <aditya.sinha@conasindia.com>
2023-07-27 17:30:27 +0530

    async

--- index.js
+++ index.js
@@ -1,15 +1,89 @@
-var a=10
-let b=20
-const c=30
+et p1;
+
+let p2;
+
+let p3;
 
 
-console.log(c);
+
+
+
+async function asynfunc(){
 
 
 
 
-// var let const
-// data types
-// hoisting
-// single threaded and multithreaded
-// synchronous and asynchronous
+    let res1 = await p1;
+
+    let res2 = await p2;
+
+    let res3 = await p3;
+
+
+
+
+
+    console.log(res1);
+
+    console.log(res2);
+
+    console.log(res3);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+asynfunc();
+
+
+
+
+
+
+
+
+function resolveAfter2Seconds() {
+
+    return new Promise(resolve => {
+
+      setTimeout(() => {
+
+        resolve('resolved');
+
+      }, 2000);
+
+    });
+
+  }
+
+  
+
+  async function asyncCall() {
+
+    console.log('calling');
+
+    const result = await resolveAfter2Seconds();
+
+    console.log(result);
+
+    // Expected output: "resolved"
+
+  }
+
+
+
+
+
+
+
+  asyncCall();
+
+
