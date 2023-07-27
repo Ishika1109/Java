37562a21b8a5e00a0ec3355d704d7ec5ab543df1
hjha2003 <hitesh.jha@conasindia.com>
2023-07-27 17:24:53 +0530

    Async functions

--- index.js
+++ index.js
@@ -4,3 +4,64 @@ const c=30
 
 
 console.log(c);
+
+function myfunction(){
+
+    alert("hello this is third push")
+}
+
+
+
+let p1;
+let p2;
+let p3;
+
+
+async function asynfunc(){
+
+    let res1 = await p1;
+    let res2 = await p2;
+    let res3 = await p3;
+
+
+    console.log(res1);
+    console.log(res2);
+    console.log(res3);
+
+
+
+
+
+}
+
+asynfunc();
+
+
+
+
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // Expected output: "resolved"
+  }
+
+
+
+
+  asyncCall();
+
+
+
+
+
+
